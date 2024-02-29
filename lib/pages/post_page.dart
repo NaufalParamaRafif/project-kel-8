3e3699aa05d3cdf3cfb076afeab5230d9dc6103a
rifai1010 <2017rifai2017@gmail.com>
2024-02-29 14:12:52 +0700

    post page

--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -4,7 +4,7 @@ class Post extends StatelessWidget {
   const Post({super.key});
 
   @override
-  //tesfsdzf
+  //tesfsdzfdfhhfd
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color.fromARGB(255, 255, 255, 255),
