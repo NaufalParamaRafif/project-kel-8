6770a3883d0ad0df9bb096d065ca9b01af065c81
DwiBuanaJaya <136419832+Dwi1562@users.noreply.github.com>
2024-02-29 14:00:40 +0700

    Menambahkan halaman profile dan halaman notifikasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:project_kelompok_8/pages/kode_otp_page.dart';
+import 'package:forum_jasa/notifications.dart';
+import 'package:forum_jasa/profile_screen.dart';
 
-void main() {
-  runApp(
-    const MaterialApp(
-      title: 'Toko',
-      home: KodeOTPPage(),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-    ),
-  );
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(255, 255, 255, 255),
+      ),
+      home: Notifications(),
+    );
+  }
 }
